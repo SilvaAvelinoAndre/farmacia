157f6c51ffb271eda3a64030889c7e4369ae196d
SilvaAvelinoAndre <andreavelinosilva@hotmail.com>
2022-04-15 15:10:21 -0300

    Primeiro commit

---
+++ Farmacia/src/main/java/br/com/farmacia/domain/Fornecedores.java
@@ -0,0 +1,47 @@
+package br.com.farmacia.domain;
+
+public class Fornecedores {
+	
+	private Long id;
+	private String descricao;
+	
+	
+	public Fornecedores() {
+		
+	}
+
+
+	public Fornecedores(Long id, String descricao) {
+		this.id = id;
+		this.descricao = descricao;
+	}
+
+
+	public Long getId() {
+		return id;
+	}
+
+
+	public void setId(long i) {
+		this.id = i;
+	}
+
+
+	
+
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	@Override
+	public String toString() {
+		return "Id do Fornecedor = " + id + ", Descricao = " + descricao + "!";
+	}
+
+}
---
+++ Farmacia/src/main/java/br/com/farmacia/factory/ConexaoFactory.java
@@ -0,0 +1,40 @@
+package br.com.farmacia.factory;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class ConexaoFactory {
+	
+	private static final String USUARIO = "root";
+	private static final String SENHA = "123456";
+	private static final String URL = "jdbc:mysql://localhost:3306/farmacia";
+	
+	
+	public static Connection conectar() throws SQLException{
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		Connection conexao = DriverManager.getConnection(URL, USUARIO, SENHA);
+	return conexao;	
+	 
+	 }
+	
+	public static void main(String[] args)     {
+		try {
+		Connection conexao = ConexaoFactory.conectar();
+		System.out.println("Conexao bem sucedida!!!");
+		 
+		}
+		catch(SQLException e) {
+			e.printStackTrace();
+			System.out.println("Falha na conexão!!!");
+		}
+	}
+	
+
+}
